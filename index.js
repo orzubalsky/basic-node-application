04ef8d3d9bedb1929eae4741c6be3435490f417f
orzubalsky <orzubalsky@gmail.com>
2022-11-07 10:04:27 -0500

    Add express app

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hi!")
+// Import the random text generator library
+import { sentence } from 'txtgen'
+
+// Import the express library and assign it to a variable
+import express from 'express'
+
+// Create an instance of an express application
+const app = express()
+
+// Set the port the application will be running on
+const port = process.env.PORT || 3001
+
+// Set up a response for the root path of the application
+app.get('/', (req, res) => {
+  const randomSentence = sentence()
+
+  res.send(randomSentence)
+})
+
+// Set the application to listen a port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
